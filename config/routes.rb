29667402e4c92eca3534fc41196a4dc95188bb85
rica213 <25f5k252l@mozmail.com>
2023-06-05 17:15:04 +0300

    Create controllers, views, and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "recipes#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  resources :foods, only: [:index, :new, :create, :destroy]
+
 end
