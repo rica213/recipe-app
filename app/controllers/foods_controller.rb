2be9dac14d3379f73d6a59d0d6159807108f24e6
shaaibu <shaaibusuleiman@gamil.com>
2023-06-06 15:21:29 +0100

    Implement functionality to add new food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,27 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = current_user.foods
+  end
 
-  def new; end
+  def new
+    @food = Food.new
+  end
 
-  def create; end
+  def create
+    @food = current_user.foods.new(food_params)
+
+    if @food.save
+      redirect_to foods_path, notice: 'Food created successfully'
+    else
+      render :new
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
