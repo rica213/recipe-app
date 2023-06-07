2acf2196ecdad158a26e9d2220c5335136f5cdad
rica213 <25f5k252l@mozmail.com>
2023-06-07 22:01:36 +0300

    Modify food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,4 +6,22 @@ class FoodsController < ApplicationController
   def create; end
 
   def destroy; end
+
+  def edit
+    @food = Food.find(params[:id])
+  end
+
+  def update
+    if @food.update(food_params)
+      redirect_to @food, notice: 'Food was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
