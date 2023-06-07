dda3520e03dbbbfdf52fbbf743b12fc32ab689ac
rica213 <25f5k252l@mozmail.com>
2023-06-07 05:29:03 +0300

    Create users controller invoke erb: create views invoke rspec: create corresponding test files invoke helper create action shopping list add shopping list to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "recipes#index"
-
+  
   resources :recipes, only: [:index, :show, :new, :create, :destroy]
   resources :foods, only: [:index, :new, :create, :destroy, :edit, :update,]
-
+  
+  get 'users/shopping_list'
   match 'public_recipes', to: "recipes#public_recipes", via: :get
 end
