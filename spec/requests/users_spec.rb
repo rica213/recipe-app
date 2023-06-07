8648ed30790008807281773930c7bcf049b5704d
rica213 <25f5k252l@mozmail.com>
2023-06-07 22:02:46 +0300

    Fix: Correct linters errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /shopping_list" do
-    it "returns http success" do
-      get "/users/shopping_list"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /shopping_list' do
+    it 'returns http success' do
+      get '/users/shopping_list'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
