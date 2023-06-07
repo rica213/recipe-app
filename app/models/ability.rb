b345ca77f909ada6b82baeb8e229f2d13f96d5a8
rica213 <25f5k252l@mozmail.com>
2023-06-07 09:55:43 +0300

    Define abilities for users

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the user here.
+    user ||= User.new # Guest user
+
+    # Any user can read recipes marked as public
+    can :read, Recipe, public: true
+    # User can manage on recipes where they are the owner
+    can :manage, Recipe, user_id: user.id
+
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+  end
+end
