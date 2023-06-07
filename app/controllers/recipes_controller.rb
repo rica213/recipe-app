e83cc1f468edc0042087dbb2bdaaf4ca294a56a0
shaaibu <shaaibusuleiman@gamil.com>
2023-06-07 12:16:29 +0100

    Implement actions for recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,32 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = current_user.recipes
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @recipe = Recipe.new
+  end
 
-  def create; end
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipes created successfully'
+    else
+      render :new
+    end
+  end
 
   def destroy; end
 
   def public_recipes; end
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
+  end
 end
