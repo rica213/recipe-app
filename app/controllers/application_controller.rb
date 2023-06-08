e290ce336345ef403590895a5000711109c63a91
rica213 <25f5k252l@mozmail.com>
2023-06-08 06:35:03 +0300

    Handle invalid URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
+  rescue_from ActionController::RoutingError, with: :handle_routing_error
+
+  def handle_routing_error
+    flash[:error] = "Invalid URL"
+    redirect_to root_path
+  end
 
   protected
 
