a1fa4eb2fd01a9fc061ef5e7fe32336693e43e38
shaaibu <shaaibusuleiman@gamil.com>
2023-06-09 10:45:46 +0100

    Implement unit test for models

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,6 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
+
+  validates :name, presence: true
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Isaac', email: 'kaplan@gmail.com', password: '123456', password_confirmation: '123456')
+    @recipe = Recipe.create(name: 'vanilla', preparation_time: 1, cooking_time: 2, description: 'One of a kind', public: true, user_id: @user.id)
+  end
+
+  it 'should be a valid recipe' do
+    expect(@recipe).to be_valid
+  end
+
+  it 'should not be valid without name' do
+    @recipe.name = nil
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'should be valid with preparation time greater than zero' do
+    @recipe.preparation_time = 1
+    expect(@recipe).to be_valid
+  end
+
+  it 'should not be valid with preparation time equal to zero' do
+    @recipe.preparation_time = 0
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'should be valid with cooking time greater than zero' do
+    @recipe.cooking_time = 1
+    expect(@recipe).to be_valid
+  end
+
+  it 'should not be valid with cooking time equal or less than zero' do
+    @recipe.cooking_time = -1
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'should not be valid without description' do
+    @recipe.description = nil
+    expect(@recipe).to_not be_valid
+  end
 end
